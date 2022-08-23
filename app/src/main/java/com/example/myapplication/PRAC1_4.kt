f3e1e7a20e6166c50af52fa3d9de957872feb3a3
hetul <hetulshah2002@gmail.com>
2022-08-23 22:26:34 +0530

    final code

---
+++ app/src/main/java/com/example/myapplication/PRAC1_4.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+fun main(){
+    print("Enter Number: ")
+    val num = readLine()!!.toInt()
+    if (num%2==0){
+        println("$num is Even")
+    }
+    else{
+        println("$num is Odd ")
+    }
+}
